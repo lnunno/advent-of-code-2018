875942f6c81f6fd9d2cab9a03bae5cb6b82f3bb4
Lucas Nunno <lucasnunno@gmail.com>
2018-12-26 10:24:44 -0700

    Day 1

--- day01/day01.ts
+++ day01/day01.ts
@@ -1,18 +1,38 @@
 import * as fs from "fs";
 import * as path from "path";
 
-const inputFilepath = path.resolve(__dirname, 'input');
-console.log(`${inputFilepath}`);
-fs.readFile(inputFilepath, 'utf8', (err, data) => {
-    if (err) {
-        console.error(`${err}`);
-        return;
-    }
-    const lines = data.split('\n');
-    let sum = 0;
+function findDuplicateFrequency(lines: string[]) {
+  let sum = 0;
+  const seenBefore = new Set([sum]);
+  while (true) {
     for (const numLine of lines) {
-        const num = Number(numLine);
-        sum += num;
+      if (numLine === "") {
+        continue;
+      }
+      const num = Number(numLine);
+      sum += num;
+      if (seenBefore.has(sum)) {
+        console.log(`Seen the frequency ${sum} before`);
+        return sum;
+      }
+      seenBefore.add(sum);
     }
-    console.log(`Answer is ${sum}`);
+  }
+}
+
+const inputFilepath = path.resolve(__dirname, "input");
+console.log(`${inputFilepath}`);
+fs.readFile(inputFilepath, "utf8", (err, data) => {
+  if (err) {
+    console.error(`${err}`);
+    return;
+  }
+  let sum = 0;
+  const lines = data.split("\n");
+  for (const numLine of lines) {
+    const num = Number(numLine);
+    sum += num;
+  }
+  console.log(`Answer is ${sum}`);
+  findDuplicateFrequency(lines);
 });
